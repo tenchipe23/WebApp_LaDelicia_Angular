532f6c16e6ebff31f7dacc4d7efce485cb914146
karl262 <carlins502@gmail.com>
2025-03-28 22:55:48 -0600

    Feat: Add SideBar to Cooking-Time.

--- WebApp_LaDelicia/src/app/features/controlPanel/horno/horno.component.ts
+++ WebApp_LaDelicia/src/app/features/controlPanel/horno/horno.component.ts
@@ -6,12 +6,14 @@ import { OvenTableComponent } from '../../../shared/tables/oven-table/oven-table
 import { ProductService } from '../../../core/services/product.service';
 import { ToastrService } from 'ngx-toastr';
 import { Modal } from "bootstrap";
+import {CopyrightComponent} from "../../../shared/copyright/copyright.component";
+import {SidebarPanelComponent} from "../../../shared/sidebar-panel/sidebar-panel.component";
 
 
 @Component({
   selector: 'app-horno',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent, OvenTableComponent],
+  imports: [CommonModule, OvenTableComponent, CopyrightComponent, SidebarPanelComponent],
   templateUrl: './horno.component.html',
   styleUrls: ['./horno.component.css']
 })
@@ -41,14 +43,14 @@ export class HornoComponent implements OnInit {
     this.productService.getAllBreads().subscribe({
       next: (response) => {
         console.log('📢 Productos obtenidos de la API secundaria:', response);
-        
+
         // Manejo de datos de respuesta de la API de forma más robusta
         if (response?.data && Array.isArray(response.data)) {
           this.products = response.data.map((bread: any) => ({
-            id: bread._id, 
+            id: bread._id,
             name_product: bread.name,
             ingredients: bread.ingredients,
-            baking_time: bread.bakingTime,  
+            baking_time: bread.bakingTime,
           }));
         } else {
           console.error('❌ Error: La respuesta no contiene un array en data');
@@ -64,15 +66,15 @@ export class HornoComponent implements OnInit {
 
   // Método para mostrar el tiempo formateado (minutos:segundos)
   getFormattedTime(): string {
-    const minutes = Math.floor(this.seconds / 60);  
-    const secs = this.seconds % 60;  
+    const minutes = Math.floor(this.seconds / 60);
+    const secs = this.seconds % 60;
     return `${minutes < 10 ? '0' : ''}${minutes}:${secs < 10 ? '0' : ''}${secs}`;
   }
 
   // Método para ingresar números desde el teclado numérico
   enterDigit(num: string | number): void {
     const number = typeof num === 'string' ? parseInt(num, 10) : num;
-    if (this.inputValue.length < 3) { 
+    if (this.inputValue.length < 3) {
       this.inputValue += number.toString();
     }
 
@@ -81,14 +83,14 @@ export class HornoComponent implements OnInit {
 
   // Función para eliminar el último dígito
   deleteLastDigit(): void {
-    this.inputValue = this.inputValue.slice(0, -1);  
+    this.inputValue = this.inputValue.slice(0, -1);
     this.updateTimeFromInput();
   }
 
 // Actualiza el tiempo desde el valor de input
 private updateTimeFromInput(): void {
   const minutes = Number(this.inputValue);
-  
+
   if (isNaN(minutes)) {
     return; // Si no es un número, no hacemos nada
   }
@@ -98,11 +100,11 @@ private updateTimeFromInput(): void {
     this.inputValue = '80'; // Establecemos el valor máximo permitido
     this.seconds = 80 * 60; // Asignamos el máximo permitido en segundos
   } else {
-    this.seconds = minutes * 60;  
+    this.seconds = minutes * 60;
   }
 
-  this.initialTime = this.seconds;  
-  this.updateTimerDisplay();    
+  this.initialTime = this.seconds;
+  this.updateTimerDisplay();
 
   // Actualizar el tiempo en la API si hay un producto en el horno
   if (this.currentProductId) {
@@ -124,10 +126,10 @@ private updateTimeFromInput(): void {
   // Método para manejar el evento de cocinar
   onCook(bakingTime: number, productId: string): void {
     this.currentProductId = productId; // Guardamos el ID del producto que se está cocinando
-    this.seconds = bakingTime * 60;  
-    this.initialTime = this.seconds;  
-    this.updateTimerDisplay();  
- 
+    this.seconds = bakingTime * 60;
+    this.initialTime = this.seconds;
+    this.updateTimerDisplay();
+
   }
 
   onCookProduct(productId: string): void {
@@ -136,21 +138,21 @@ private updateTimeFromInput(): void {
       this.onCook(product.baking_time, product.id);  // Inicia el temporizador con el tiempo de horneado del producto
     }
   }
-  
+
 
   startTimer(): void {
     if (!this.isRunning && this.seconds > 0) {
       this.isRunning = true;
       this.timer = setInterval(() => {
         if (this.seconds > 0) {
-          this.seconds--;  
-          this.updateTimerDisplay();  
+          this.seconds--;
+          this.updateTimerDisplay();
 
           if (this.seconds <= this.initialTime / 2) {
             this.timerValue = `<span style="color: yellow">${this.getFormattedTime()}</span>`;
           }
         } else {
-          this.stopTimer();  
+          this.stopTimer();
         }
       }, 1000);
     }
@@ -161,7 +163,7 @@ private updateTimeFromInput(): void {
   stopTimer(): void {
     if (this.isRunning) {
       this.isRunning = false;
-      clearInterval(this.timer);  
+      clearInterval(this.timer);
     }
   }
 
@@ -171,8 +173,8 @@ private updateTimeFromInput(): void {
   //   this.isRunning = false;
   //   clearInterval(this.timer);
   //   this.seconds = 0;
-  //   this.inputValue = '';  
-  //   this.updateTimerDisplay();  
+  //   this.inputValue = '';
+  //   this.updateTimerDisplay();
   // }
 
   private updateTimerDisplay(): void {
@@ -182,21 +184,21 @@ private updateTimeFromInput(): void {
   }
 
   getRedButtonClass(): string {
-    return this.isRunning ? 'red-button opaque' : 'red-button';  
+    return this.isRunning ? 'red-button opaque' : 'red-button';
   }
 
   getGreenButtonRgbClass(): string {
-    return this.isRunning ? 'green-button-rgb active' : 'green-button-rgb';  
+    return this.isRunning ? 'green-button-rgb active' : 'green-button-rgb';
   }
 
   getFinishedButtonClass(): string {
-    return !this.isRunning && this.seconds === 0 ? 'green-button finish' : 'green-button';  
+    return !this.isRunning && this.seconds === 0 ? 'green-button finish' : 'green-button';
   }
 
 
   confirmDelete(productId: number): void {
       this.productIdToDelete = productId;
-  
+
       // Abre la modal (id="deleteModal") con la API de Bootstrap
       const modalElement = document.getElementById('deleteModal');
       if (modalElement) {
@@ -204,10 +206,10 @@ private updateTimeFromInput(): void {
         modalBootstrap.show();
       }
     }
-  
+
     deleteProduct(): void {
       if (!this.productIdToDelete) return;
-  
+
       this.productService.deleteBread(this.productIdToDelete.toString()).subscribe({
         next: () => {
           // Cerrar modal
@@ -234,19 +236,19 @@ private updateTimeFromInput(): void {
         this.toastr.warning('No se puede iniciar otro horneado hasta que termine el temporizador en curso.', 'Por favor espere.');
         return;
       }
-    
+
       const product = this.products.find(p => p.id);
       if (!product) {
         this.toastr.error('No se encontró un producto para hornear.', 'Error');
         return;
       }
-    
+
       this.productService.startBaking(product.id).subscribe(
         response => {
           console.log('Respuesta del servidor:', response);
           this.toastr.success('Horneado iniciado con éxito.');
-          
-          this.startTimer(); 
+
+          this.startTimer();
         },
         error => {
           console.error('Error en la API:', error);
@@ -254,5 +256,5 @@ private updateTimeFromInput(): void {
         }
       );
     }
-    
+
 }
