f98d142e350e19ddaba56a2c1ca83e64088c0c3b
Roberto-H-Z <20233l001058@gmail.com>
2025-03-29 21:59:35 -0600

    Fix: Corregir funcionalidad de la interfaz de el horno

--- WebApp_LaDelicia/src/environments/environment.ts
+++ WebApp_LaDelicia/src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3100/api',
-  authServiceUrl: 'http://localhost:3001/api/auths',
+  authServiceUrl: 'http://localhost:3100/api/auths',
   userServiceUrl: 'http://localhost:3100/api/users',
   clientServiceUrl: 'http://localhost:3100/api/users',
   employeeServiceUrl: 'http://localhost:3100/api/employees',
   productServiceUrl: 'http://localhost:3100/api/products',
   arduinoServiceUrl: 'http://localhost:3000/api/bread',
-  arduinoServiceUrlAux: 'http://192.168.68.250:3000/api/bread',
+  arduinoServiceUrlAux: 'http://localhost:3000/api/bread',
 };
